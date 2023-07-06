ba848bad6cf9d38c762a339ac4a68cdd811fa51a
Karanpreet Singh <singhkaranpreet710@gmail.com>
2023-07-06 23:23:13 +0530

    fixed request error
    Signed-off-by: Karanpreet Singh <singhkaranpreet710@gmail.com>

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -2,11 +2,14 @@ import Post from "../post/Post";
 import "./posts.scss";
 import { useQuery } from "react-query";
 import {makeRequest} from "../../axios";
+import { useContext } from "react";
 
-const Posts = ({userId}) => {
+const Posts = () => {
+
+  const {currentUser} = useContext(AuthContext);
 
   const {isLoading, error, data} = useQuery('posts',()=>
-    makeRequest.get("/posts?userId=" + userId).then((res)=>{
+    makeRequest.get("/posts?userId=" + currentUser.id).then((res)=>{
       return res.data; 
     })
   )
