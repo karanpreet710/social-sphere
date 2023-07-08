c376e27c46caa54fbf49a3cb7e808afee179c74e
Karanpreet Singh <singhkaranpreet710@gmail.com>
2023-07-08 15:45:22 +0530

    changed baseURL
    Signed-off-by: Karanpreet Singh <singhkaranpreet710@gmail.com>

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL:"https://socialsphere-backend.onrender.com/api",
+    baseURL:"https://socialsphere-backend.vercel.app/api",
     withCredentials:true
 })
