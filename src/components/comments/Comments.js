bcdb950b8d558480bff8ed2d9463bf4dad7d1f70
Karanpreet Singh <singhkaranpreet710@gmail.com>
2023-07-09 13:18:00 +0530

    img fixes
    Signed-off-by: Karanpreet Singh <singhkaranpreet710@gmail.com>

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -38,7 +38,7 @@ const Comments = ({postId}) => {
   return (
     <div className="comments">
       <div className="write">
-        <img src={currentUser.profilePic} alt="" />
+        <img src={"./upload/" + currentUser.profilePic} alt="" />
         <input type="text" placeholder="write a comment" value={desc} onChange={e=>setDesc(e.target.value)}/>
         <button onClick={handleClick}>Send</button>
       </div>
