8f238b8cd3c5a44f0b7fbe672f31792e8547af0f
Karanpreet Singh <singhkaranpreet710@gmail.com>
2023-07-09 13:08:52 +0530

    changed axios req
    Signed-off-by: Karanpreet Singh <singhkaranpreet710@gmail.com>

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-    baseURL:"https://https://api-socialsphere.vercel.app/api",
+    baseURL:"https://api-socialsphere.vercel.app/api",
     withCredentials:true
 })
