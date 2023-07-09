fe17462e0a870ff14c6a6c18bfe089d6a3a25830
Karanpreet Singh <singhkaranpreet710@gmail.com>
2023-07-09 15:54:50 +0530

    fixed home page error
    Signed-off-by: Karanpreet Singh <singhkaranpreet710@gmail.com>

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,16 +2,13 @@ import Stories from "../../components/stories/Stories"
 import Posts from "../../components/posts/Posts"
 import "./home.scss"
 import Share from "../../components/share/Share"
-import { useContext } from "react"
-import { AuthContext } from "../../context/authContext"
 
 const Home = () => {
-  const {currentUser} = useContext(AuthContext);
   return (
     <div className="home">
       <Stories />
       <Share />
-      <Posts userId = {currentUser.id}/>
+      <Posts />
     </div>
   )
 }
